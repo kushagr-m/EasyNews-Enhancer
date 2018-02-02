5de363e232409bfe2ff44e84ef8b9521c3211c00
Kushagr Mittal <kushagr.mittal@live.com>
2018-02-02 23:29:02 +1100

    Cleaned up
    Removed excess whitespace, added a few comments, added more metadata and icon.

--- EasyNews-Enhancer.user.js
+++ EasyNews-Enhancer.user.js
@@ -1,32 +1,34 @@
 // ==UserScript==
 // @name	NHK Easy News Enhancer
 // @namespace	https://github.com/theKKCD/EasyNews-Enhancer
-// @version	0.9
-// @description	Userscript to enhance readability and usability on NHK Easy News.
+// @version	10
+// @description	Userscript to enhance readability and usability of NHK Easy News, including adding the ability to toggle furigana.
 // @author	Kushagr M (theKKCD)
 // @include	http://www3.nhk.or.jp/news/easy/*
 // @require	http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @grant	none
+// @icon 	https://github.com/theKKCD/EasyNews-Enhancer/raw/master/Files/icon.ico
+// @supportURL	https://github.com/theKKCD/EasyNews-Enhancer/issues
+// @updateURL	https://raw.githubusercontent.com/theKKCD/EasyNews-Enhancer/master/EasyNews-Enhancer.user.js
 // @downloadURL	https://raw.githubusercontent.com/theKKCD/EasyNews-Enhancer/master/EasyNews-Enhancer.user.js
 // ==/UserScript==
 
 var githubRepo = "https://github.com/theKKCD/EasyNews-Enhancer";
-
 
 var labelHide = 'Hide ふりがな';
 var labelShow = 'Show ふりがな';
 
 var furiVisible = true;
 
-if (window.location.pathname.indexOf("/news/easy/k")==0) { 
+if (window.location.pathname.indexOf("/news/easy/k")==0) { // if on an article page
 	$("#soundkana").prepend('<button type="button" id="furigToggleButton" style="display:block;height:46px;width:213px;margin-left:30px;border-radius:9px;background-color:#FF921D;color:white;font-size:16px;font-family:Noto Sans CJK JP">' + labelHide + '</button>');
-} else {
+} else { // else on homepage
 	$("#content").prepend('<button type="button" id="furigToggleButton" style="margin:auto;width:90%;border-radius:5px;display:block;height:46px;margin-bottom:8px;font-size:16px;background-color:#FF921D;color:white;font-family:Noto Sans CJK JP">' + labelHide + '</button>');
 }
 
 $("body").css({"font-family":"Noto Sans CJK JP"}); // replace font
 
-// PAGE CLEANUP
+// Junk removal
 
 var regNewsLink = $('#regularnews > p > a').attr('href'); // store link to full article
 $("#regularnews").remove();	// remove full article button
@@ -49,7 +51,7 @@ $("#enq_ansbak").remove();
 
 $("#footer").prepend('<p class="copyright">NHK Easy News Enhancer, by Kushagr M (theKKCD). <a href="' + githubRepo + '">Contribute on Github.</a></p>');
 
-// FURIGANA TOGGLE BUTTON
+// Furigana button toggle code
 
 function furigHide(){ 
 	if (furiVisible == true) {
@@ -60,13 +62,7 @@ function furigHide(){
   	$("rt").css({ color: "#333333" });
 		$('#furigToggleButton').text(labelHide);
     furiVisible = true;
-   
   }
 }
 
 document.getElementById("furigToggleButton").addEventListener("click", furigHide);
-document.addEventListener("keypress", function(e){
-  if (e.keyCode == 70) {
-    furigHide();
-  }
-}, false);
