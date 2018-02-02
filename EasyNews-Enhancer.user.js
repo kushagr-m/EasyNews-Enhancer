da10699d702dee5b0e82f86f4672f9010bd21944
Kushagr Mittal <kushagr.mittal@live.com>
2018-02-03 03:31:48 +1100

    big update
    added:
    - embedded html5 audio! (instead of flash which didn't even show up except in IE)
    - remove lots more garbage (empty <p> tags)
    - re added in 'full news article' link now at bottom of article (with furigana!)
    - custom font is now optional

--- EasyNews-Enhancer.user.js
+++ EasyNews-Enhancer.user.js
@@ -1,57 +1,25 @@
 // ==UserScript==
-// @name	NHK Easy News Enhancer
-// @namespace	https://github.com/theKKCD/EasyNews-Enhancer
-// @version	10
-// @description	Userscript to enhance readability and usability of NHK Easy News, including adding the ability to toggle furigana.
-// @author	Kushagr M (theKKCD)
-// @include	http://www3.nhk.or.jp/news/easy/*
-// @require	http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
-// @grant	none
-// @icon 	https://github.com/theKKCD/EasyNews-Enhancer/raw/master/Files/icon.png
-// @supportURL	https://github.com/theKKCD/EasyNews-Enhancer/issues
-// @updateURL	https://raw.githubusercontent.com/theKKCD/EasyNews-Enhancer/master/EasyNews-Enhancer.user.js
-// @downloadURL	https://raw.githubusercontent.com/theKKCD/EasyNews-Enhancer/master/EasyNews-Enhancer.user.js
+// @name	      NHK Easy News Enhancer
+// @namespace	  https://github.com/theKKCD/EasyNews-Enhancer
+// @version	    12
+// @description	Enhances the usability, readability and UX of NHK Easy News. Adds Furigana toggling, HTML5 audio and reduces clutter.
+// @author      Kushagr M (theKKCD)
+// @include	    http://www3.nhk.or.jp/news/easy/*
+// @require	    http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @grant	      none
+// @icon        https://github.com/theKKCD/EasyNews-Enhancer/raw/master/Files/icon.png
+// @supportURL  https://github.com/theKKCD/EasyNews-Enhancer/issues
+// @updateURL   https://raw.githubusercontent.com/theKKCD/EasyNews-Enhancer/master/EasyNews-Enhancer.user.js
+// @downloadURL https://raw.githubusercontent.com/theKKCD/EasyNews-Enhancer/master/EasyNews-Enhancer.user.js
 // ==/UserScript==
 
 var githubRepo = "https://github.com/theKKCD/EasyNews-Enhancer";
 
+// furigana toggle vars and fn
+
 var labelHide = 'Hide ふりがな';
 var labelShow = 'Show ふりがな';
-
 var furiVisible = true;
-
-if (window.location.pathname.indexOf("/news/easy/k")==0) { // if on an article page
-	$("#soundkana").prepend('<button type="button" id="furigToggleButton" style="display:block;height:46px;width:213px;margin-left:30px;border-radius:9px;background-color:#FF921D;color:white;font-size:16px;font-family:Noto Sans CJK JP">' + labelHide + '</button>');
-} else { // else on homepage
-	$("#content").prepend('<button type="button" id="furigToggleButton" style="margin:auto;width:90%;border-radius:5px;display:block;height:46px;margin-bottom:8px;font-size:16px;background-color:#FF921D;color:white;font-family:Noto Sans CJK JP">' + labelHide + '</button>');
-}
-
-$("body").css({"font-family":"Noto Sans CJK JP"}); // replace font
-
-// Junk removal
-
-var regNewsLink = $('#regularnews > p > a').attr('href'); // store link to full article
-$("#regularnews").remove();	// remove full article button
-
-$("div.header").remove(); // remove white header
-$(".contentWrap").css({"padding-top":"20px"}); // replace with blank space
-
-$("#bottom").remove(); // remove explanation text before footer
-
-$("#mainimg a").remove(); //remove javascript video button
-if ($("#mainimg").html()==""){
-	$("#mainimg").remove(); 
-}
-
-$(".pagetop").remove(); //remove goto top button
-
-$("#survey").remove(); // remove all survey content at bottom of page
-$("#enq_answer_disp").remove();
-$("#enq_ansbak").remove();
-
-$("#footer").prepend('<p class="copyright">NHK Easy News Enhancer, by Kushagr M (theKKCD). <a href="' + githubRepo + '">Contribute on Github.</a></p>');
-
-// Furigana button toggle code
 
 function furigHide(){ 
 	if (furiVisible == true) {
@@ -64,5 +32,56 @@ function furigHide(){
     furiVisible = true;
   }
 }
+
+// custom font
+var customFont = 'Noto Sans CJK JP';
+var allFonts = 'font-family:"'+ customFont + '","ヒラギノ角ゴ Pro W3","Hiragino Kaku Gothic Pro W3","メイリオ",Meiryo,"ＭＳ Ｐゴシック",Arial,verdana,sans-serif;';
+$("body").attr("style",allFonts);
+
+// if on index.html
+if (window.location.pathname == "/news/easy/index.html") {
+  var buttonStyle = 'margin:auto; width:90%; border-radius:5px; display:block; height:46px; margin-bottom:8px; font-size:16px; background-color:#FF921D; color:white; font-family:'+ customFont;
+  $("#content").prepend('<button type="button" id="furigToggleButton" style="'+ buttonStyle +'">' + labelHide + '</button>');  
+}
+
+// if on the article page
+if (window.location.pathname.indexOf("/news/easy/k") != -1) {
+  
+  // add furigana toggle
+  var buttonStyle = 'display:block; height:46px; width:213px; margin-left:30px; border-radius:9px; background-color:#FF921D; color:white; font-size:16px; font-family:'+ customFont;
+  $("#soundkana").prepend('<button type="button" id="furigToggleButton" style="'+ buttonStyle +'">' + labelHide + '</button>');
+  
+  $("#sound").remove(); // remove flash sound player
+  $("#nhk_audioplayer").remove();
+  $("#mainimg a").remove(); // remove flash video button
+  
+  // replace with html5 audio
+  var audioLocation = 'http://www3.nhk.or.jp' + window.location.pathname.replace(".html", ".mp3"); // get the location of the corresponding audio for the article
+  $("#mainimg").append('<br>' + '<audio controls style="margin:auto;padding-top:10px;width:384px"><source src="' + audioLocation + '" type="audio/mp3">There should be HTML5 audio here.</audio>');
+  
+  // replace 'normal article' buttom with unobtrusive link
+  $("#newsarticle").append('<a style="font-size:14px;text-align:right;display:block;padding-bottom:10px;text-decoration:underline;" href="'+ $('#regularnews > p > a').attr('href') +'"><ruby>普通<rt>ふつう</rt></ruby>のニュースを<ruby>読<rt>よ</rt></ruby>む</a>');
+  $("#regularnews").remove();
+  
+  $('p:empty').remove(); // remove black <p></p> at end of content ONLY on article as it breaks weather warnings
+}
+
+// junk removal
+
+
+$("div.header").remove(); // remove white header
+$("body").css({"padding-top":"20px"}); // replace with blank space
+
+$(".pagetop").remove(); //remove goto top button
+
+$("#bottom").remove(); // remove explanation text before footer
+
+$("#survey").remove(); // remove all survey content at bottom of page
+$("#enq_answer_disp").remove();
+$("#enq_ansbak").remove();
+
+$("#footer").prepend('<p class="copyright" style="font-size:14px;"><a href="' + githubRepo + '">NHK Easy News Enhancer, by Kushagr M (theKKCD). Contribute/Report bugs on Github.</a></p>');
+
+// Add event listener for furigana button.
 
 document.getElementById("furigToggleButton").addEventListener("click", furigHide);
